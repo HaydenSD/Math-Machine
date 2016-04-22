77684ecb0ff71fa5d8f100a5ea777eb6d9693663
HaydenSD <haydensd@protonmail.ch>
2016-04-22 05:18:21 -0700

    Added ASCII art to the updater

--- update.sh
+++ update.sh
@@ -7,6 +7,12 @@ if [ "$confirm" = "y" ]; then
   git pull --rebase --stat origin master
     if [ $? -eq 0 ]; then
       echo "Math Machine has been updated!"
+      printf '_____          __  .__         _____                .__    .__                  '
+      printf '/     \ _____ _/  |_|  |__     /     \ _____    ____ |  |__ |__| ____   ____    '
+      printf '/  \ /  \\__  \\   __\  |  \   /  \ /  \\__  \ _/ ___\|  |  \|  |/    \_/ __ \  '
+      printf '/    Y    \/ __ \|  | |   Y  \ /    Y    \/ __ \\  \___|   Y  \  |   |  \  ___/ '
+      printf '\____|__  (____  /__| |___|  / \____|__  (____  /\___  >___|  /__|___|  /\___  >'
+      printf '\/     \/          \/          \/     \/     \/     \/        \/     \/         '
     else
       echo "Math Machine couldn't update. Try again later?"
     fi
