c1efd9409b1ab0e966fa2b3593b67532706fa354
HaydenSD <haydensd@protonmail.ch>
2016-04-22 09:52:23 -0700

    Changed one line

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ if [ "$confirm" = "y" ]; then
   echo "Updating"
   git pull --rebase --stat origin master
     if [ $? -eq 0 ]; then
-      echo "Math Machine has been updated!"
+      echo "Math Machine has been updated or is at the current version!"
       exit
     else
       echo "Math Machine couldn't update. Try again later?"
